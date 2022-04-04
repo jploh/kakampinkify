d1f6d9a99c25c20b9021de8d43aa100cd8919c23
JP Loh <jploh@Mac-mini.local>
2022-04-04 08:32:36 +0800

    Implement own keyboard, move input name character data to var, remove daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,35 @@
 module.exports = {
     content: ["./src/**/*.{html,js}", "./dist/*.html"],
     theme: {
-        colors: {
-            "kakampink-bg": "#ffe4f3",
-            "kakampink-fg": "#be1f77",
-            "kakampink-fg2": "bE558f"
-        },
         fontFamily: {
             sans: ['Montserrat', 'Verdana', 'sans-serif']
         },
         extend: {
+            colors: {
+                "kakampink-bg": "#ffe4f3",
+                "kakampink-bg2": "#ff8c9d",
+                "kakampink-fg": "#be1f77",
+                "kakampink-fg2": "#bE558f",
+                "kakampink-fg3": "#ffbbce",
+                "kakampink-fg4": "#e72792",
+                "kakampink-accent": "#b5d7ac",
+                "kakampink-accent2": "#e72792",
+                "kakampink-accent3": "#a4e393"
+            },
+            boxShadow: {
+                "flat1": "4px 4px 0 #ff8c9d",
+                "flat2": "4px 4px 0 #ffbbce",
+                "flat3": "4px 4px 0 #cd2e6c",
+                "flat4": "4px 4px 0 #c63671"
+            },
             spacing: {
                 "128": "32rem"
             }
         },
     },
     plugins: [
-        require('@tailwindcss/forms'),
-        require('daisyui')
+        require('@tailwindcss/forms')//,
+        //require('daisyui')
     ],
     daisyui: {
         themes: [
